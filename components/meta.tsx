b19c1970687014c315cb8d624bc0c01e20149193
noy4 <naoyan418@gmail.com>
2021-05-10 08:11:37 +0900

    fix: favicon

--- components/meta.tsx
+++ components/meta.tsx
@@ -35,17 +35,7 @@ const Meta = () => {
       />
       <meta name="theme-color" content="#ffffff" />
       {/* 
-      <link
-        rel="mask-icon"
-        href={basePath + '/favicon/safari-pinned-tab.svg'}
-        color="#000000"
-      />
-      <link rel="shortcut icon" href={basePath + '/favicon/qwerty-log.png'} />
       <meta name="msapplication-TileColor" content="#000000" />
-      <meta
-        name="msapplication-config"
-        content={basePath + '/favicon/browserconfig.xml'}
-      />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={basePath + '/favicon/feed.xml'} /> */}
       <meta
